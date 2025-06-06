6c762eeac244a50f46a83919d4612890cec287aa
tattiii8 <t.yoshimura@lesure.net>
2025-06-06 22:08:44 +0900

    v2

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify(message="Hello from Heroku!")
+    return jsonify(message="Hello from Heroku! v2")
 
 @app.route("/api")
 def api():
-    return jsonify(status="ok", data={"message": "This is your API!"})
+    return jsonify(status="ok", data={"message": "This is your API v2!"})
 
 if __name__ == "__main__":
     app.run()
